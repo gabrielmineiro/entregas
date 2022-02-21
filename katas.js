2fbee4843ea5d11d95ca49db498b9a44bdd9fc11
gabriel <gabrielmineironogueira@gmail.com>
2022-02-21 19:19:09 -0300

    entrega loop e teste de mesa

---
+++ katas.js
@@ -0,0 +1,46 @@
+//RUBRICA ITEM 1
+    //for(let i =1; i < 21; i++){console.log(i)}
+
+//RUBRICA ITEM 2
+    /*for( let i = 1; i<=20; i++) {if(i % 2==0){console.log(i)}
+    else{""} }*/
+
+//RUBRICA ITEM 3
+   /*for( let i = 1; i<=20; i++) {if(i % 2!=0){console.log(i)}
+    else{""} }*/
+
+//RUBRICA ITEM 4
+  /*let fator=5
+
+    for(let i =1; i*fator<=100; i++){console.log(i*fator)}*/
+
+
+//RUBRICA ITEM 5
+    /*let expo=2
+    for(let i=1; i**expo<=100;i++){console.log(i**expo)}*/
+
+//RUBRICA ITEM 6
+    //for(let i =20; i > -1; i--){console.log(i)}
+
+//RUBRICA ITEM 7
+    /*for( let i = 20; i > -1; i--) {if(i % 2==0){console.log(i)}
+    else{""}}*/
+
+//RUBRICA ITEM 8
+    /*for( let i = 20; i>=0; i--) {if(i % 2!=0){console.log(i)}
+    else{""} }*/
+    
+//RUBRICA ITEM 9
+    let fator=5
+
+    for(let i =20; i*fator>=5; i--){console.log(i*fator)}
+
+//RUBRICA ITEM 10
+    /*let expo=2
+    for(let i=10; i**expo>=1;i--){console.log(i**expo)}*/
+
+
+
+
+
+
